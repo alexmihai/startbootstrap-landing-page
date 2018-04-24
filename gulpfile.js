9c06daffb5a9b67405fc2fb2a55ffe38c1b6b0db
Alex Paraschiv <aparaschiv@tremend.ro>
2018-04-24 16:28:34 +0300

    - formatted gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,65 +9,65 @@ var browserSync = require('browser-sync').create();
 var pjson = require('./package.json');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', function () {
 
-  // Font Awesome
-  gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
+    // Font Awesome
+    gulp.src([
+        './node_modules/font-awesome/**/*',
+        '!./node_modules/font-awesome/{less,less/*}',
+        '!./node_modules/font-awesome/{scss,scss/*}',
+        '!./node_modules/font-awesome/.*',
+        '!./node_modules/font-awesome/*.{txt,json,md}'
     ])
-    .pipe(gulp.dest('./vendor/font-awesome'))
+        .pipe(gulp.dest('./vendor/font-awesome'))
 
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+    // jQuery
+    gulp.src([
+        './node_modules/jquery/dist/*',
+        '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'))
+        .pipe(gulp.dest('./vendor/jquery'))
 
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
+    // jQuery Easing
+    gulp.src([
+        './node_modules/jquery.easing/*.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+        .pipe(gulp.dest('./vendor/jquery-easing'))
 
-  // Simple Line Icons
-  gulp.src([
-      './node_modules/simple-line-icons/fonts/**',
+    // Simple Line Icons
+    gulp.src([
+        './node_modules/simple-line-icons/fonts/**',
     ])
-    .pipe(gulp.dest('./vendor/simple-line-icons/fonts'))
+        .pipe(gulp.dest('./vendor/simple-line-icons/fonts'))
 
-  gulp.src([
-      './node_modules/simple-line-icons/css/**',
+    gulp.src([
+        './node_modules/simple-line-icons/css/**',
     ])
-    .pipe(gulp.dest('./vendor/simple-line-icons/css'))
+        .pipe(gulp.dest('./vendor/simple-line-icons/css'))
 
 });
 
 // Compile SCSS
-gulp.task('css:compile', function() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+gulp.task('css:compile', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass.sync({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
-  return gulp.src([
-      './css/*.css',
-      '!./css/*.min.css'
+gulp.task('css:minify', ['css:compile'], function () {
+    return gulp.src([
+        './css/*.css',
+        '!./css/*.min.css'
     ])
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 });
 
 // CSS
@@ -77,23 +77,23 @@ gulp.task('css', ['css:compile', 'css:minify']);
 gulp.task('default', ['css', 'vendor']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
 // Dev task
-gulp.task('dev', ['css', 'browserSync'], function() {
+gulp.task('dev', ['css', 'browserSync'], function () {
     gulp.watch('./scss/*.scss', ['css']);
     gulp.watch('./*.html', browserSync.reload);
 });
 
 
 // Create dist task
-gulp.task('dist', ['default'], function() {
+gulp.task('dist', ['default'], function () {
     // images
     gulp.src([
         './img/**'
@@ -120,7 +120,7 @@ gulp.task('dist', ['default'], function() {
 });
 
 // release zip
-gulp.task ('release', ['dist'], function () {
+gulp.task('release', ['dist'], function () {
     gulp.src('dist/*')
         .pipe(zip(pjson.name + '-' + pjson.version + '.zip'))
         .pipe(gulp.dest('releases'))
