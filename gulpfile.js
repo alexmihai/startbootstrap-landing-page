212bb94b1f5cab332ac028769ec95aee0fec3426
Alex Paraschiv <aparaschiv@tremend.ro>
2018-04-24 16:20:17 +0300

    - added dist task

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,8 @@ var rename = require("gulp-rename");
 var pkg = require('./package.json');
 var browserSync = require('browser-sync').create();
 
-// Set the banner content
-var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  ''
-].join('');
-
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function() {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
 
   // Font Awesome
   gulp.src([
@@ -102,6 +85,34 @@ gulp.task('browserSync', function() {
 
 // Dev task
 gulp.task('dev', ['css', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./*.html', browserSync.reload);
+    gulp.watch('./scss/*.scss', ['css']);
+    gulp.watch('./*.html', browserSync.reload);
+});
+
+
+// Create dist task
+gulp.task('dist', ['default'], function() {
+    // images
+    gulp.src([
+        './img/**'
+    ])
+        .pipe(gulp.dest('./dist/img/'));
+
+    // index.html
+    gulp.src([
+        './index.html'
+    ])
+        .pipe(gulp.dest('./dist/'));
+
+    // css
+    gulp.src([
+        './css/*.min.css'
+    ])
+        .pipe(gulp.dest('./dist/css/'));
+
+    // vendor
+    gulp.src([
+        './vendor/**'
+    ])
+        .pipe(gulp.dest('./dist/vendor/'));
 });
